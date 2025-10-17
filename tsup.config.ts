8e87e8bb13a26292bf287aa5314586d8b59902b3
sangreprimitiva <juan.sangreprimitiva@gmail.com>
2025-10-17 16:13:55 -0300

    :bug: fix(build): force .mjs extension for ESM bundle to match package exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,4 @@
+// tsup.config.ts o tsup.config.js
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
@@ -7,5 +8,12 @@ export default defineConfig({
 	sourcemap: true,
 	clean: true,
 	target: 'es2020',
-	minify: false
+	minify: false,
+	outDir: 'dist',
+	splitting: false,
+	outExtension({ format }) {
+		return {
+			js: format === 'esm' ? '.mjs' : '.cjs'
+		};
+	}
 });
